d5325eab2bd2621a27e980a3e0afa792329743e1
Benjamin Dos Santos <benj.ds@gmail.com>
2012-11-05 12:55:54 +0100

    Use two-space soft tabs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,11 @@
-class papertrail($host = 'logs.papertrailapp.com', 
-                 $port = '12839',
-                 $cert_url = 'https://papertrailapp.com/tools/syslog.papertrail.crt',
-                 $cert = '/etc/syslog.papertrail.crt',
-                 $optional_files = []) {
-    
-  include papertrail::install, 
+class papertrail(
+  $host = 'logs.papertrailapp.com',
+  $port = '12839',
+  $cert_url = 'https://papertrailapp.com/tools/syslog.papertrail.crt',
+  $cert = '/etc/syslog.papertrail.crt',
+  $optional_files = []
+  ) {
+
+  include papertrail::install,
           papertrail::service
 }
